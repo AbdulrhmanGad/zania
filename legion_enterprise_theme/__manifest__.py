d06aebb6803dd3854d6dcb2de878f9d6337a096c
Abdulrhman <obeidgad@gmail.com>
2021-07-03 15:43:49 +0200

    [ADD]

---
+++ legion_enterprise_theme/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'Odoo Enterprise Theme',
+    'version': '14.0.1.0.1',
+    'summary': 'Odoo Enterprise Theme',
+    'author': 'Bytelegion',
+    'license': 'AGPL-3',
+    'maintainer': 'Bytelegion',
+    'company': 'Bytelegion',
+    'website': 'https://bytelegions.com',
+    'depends': [
+        'web'
+    ],
+    'category':'Branding',
+    'description': """
+           Odoo Enterprise Theme
+    """,
+   'data': [
+
+    'views/webclient_template_extend.xml',
+
+    ],
+    'price':0,
+    'currency':'USD',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'images': ['static/description/icon.png','static/description/main_screenshot.png']
+}
