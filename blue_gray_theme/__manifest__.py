38850c0a38f56755f13fae8b23619562e2443176
Abdulrhman <obeidgad@gmail.com>
2021-07-03 12:12:24 +0200

    [ADD]

---
+++ blue_gray_theme/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Blue & Gray Theme',
+    'version': '14.0',
+    'summary': 'Blue & Gray Theme',
+    'author': 'Mahmoud Nayel',
+    'license': 'AGPL-3',
+    'maintainer': 'Mahmoud Nayel',
+    'company': 'Mahmoud Nayel',
+    'support': "mah.abo.nayel@gmail.com",
+    'website': 'https://www.linkedin.com/in/mahmoudnayel/',
+    'depends': [
+        'web'
+    ],
+    'category': 'Technical',
+    'description': """
+           Blue & Gray Theme
+    """,
+    'data': [
+        'views/webclient_template_extend.xml',
+    ],
+    'price': 0,
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'images': ['static/description/icon.png']
+}
